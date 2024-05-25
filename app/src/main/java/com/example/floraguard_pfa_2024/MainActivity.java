cb76e0f6dc22ff2ed92d0a588c45b70ca217c222
AElKaimouni <112639323+AElKaimouni@users.noreply.github.com>
2024-05-25 22:58:51 +0100

    implement PlantModel

--- app/src/main/java/com/example/floraguard_pfa_2024/MainActivity.java
+++ app/src/main/java/com/example/floraguard_pfa_2024/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.floraguard_pfa_2024.Plant.PlantInterface;
+import com.example.floraguard_pfa_2024.Plant.PlantModel;
+import com.example.floraguard_pfa_2024.Plant.PlantType;
 import com.example.floraguard_pfa_2024.User.UserInterface;
 import com.example.floraguard_pfa_2024.User.UserModel;
 
@@ -14,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/example/floraguard_pfa_2024/Plant/PlantModel.java
+++ app/src/main/java/com/example/floraguard_pfa_2024/Plant/PlantModel.java
@@ -1,4 +1,40 @@
 package com.example.floraguard_pfa_2024.Plant;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class PlantModel {
+    private String id;
+    private String name;
+    private String image;
+    private PlantType type;
+
+    public PlantModel(String id, String name, String image, String type) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.type = PlantType.fromDescription(type);
+    }
+
+    protected static final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    protected static final FirebaseFirestore db = FirebaseFirestore.getInstance();;
+    protected static final FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public PlantType getType() {
+        return this.type;
+    }
+
 }
