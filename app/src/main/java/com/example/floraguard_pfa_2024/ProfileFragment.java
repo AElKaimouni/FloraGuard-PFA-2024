7d28205e6432462efab7a51c4c751d3f553ea15c
safouat <elyassinisafouat2@gmail.com>
2024-05-25 21:59:40 +0300

    new changes

--- app/src/main/java/com/example/floraguard_pfa_2024/ProfileFragment.java
+++ app/src/main/java/com/example/floraguard_pfa_2024/ProfileFragment.java
@@ -1,42 +1,36 @@
 package com.example.floraguard_pfa_2024;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import androidx.fragment.app.Fragment;
+import com.example.floraguard_pfa_2024.User.UserInterface;
+import com.example.floraguard_pfa_2024.User.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private EditText name, password, Newpassword;
+    private Button buttonSaveName, buttonPassword, buttonUploadImage, btnSaveAvatar;
+    private ImageView image;
+    private FirebaseAuth mAuth;
+    private String email;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -58,7 +52,58 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            email = user.getEmail();
+        }
+
+        name = rootView.findViewById(R.id.ed_name);
+        buttonSaveName = rootView.findViewById(R.id.btn_save_name);
+        buttonSaveName.setOnClickListener(v -> updateName());
+
+        password = rootView.findViewById(R.id.ed_new_password);
+        Newpassword = rootView.findViewById(R.id.ed_confirm_password);
+        buttonPassword = rootView.findViewById(R.id.btn_save_password);
+        buttonPassword.setOnClickListener(v -> updatePassword());
+
+        image = rootView.findViewById(R.id.img_avatar);
+        buttonUploadImage = rootView.findViewById(R.id.btn_upload_avatar);
+        btnSaveAvatar = rootView.findViewById(R.id.btn_save_avatar);
+        btnSaveAvatar.setOnClickListener(v -> updateImage());
+
+        return rootView;
+    }
+
+    private void updateImage() {
+        if (image.getDrawable() != null) {
+            UserInterface.findOne(email).thenAccept(res -> {
+                res.setAvatar(image.toString()).update().thenAccept(v -> {
+                    Log.d("user-update", "user updated " + res.getEmail());
+                });
+            });
+        }
+    }
+
+    private void updatePassword() {
+        if (password.getText().toString().equals(Newpassword.getText().toString())) {
+            UserInterface.findOne(email).thenAccept(res -> {
+                res.setName(name.getText().toString()).setAvatar(image.toString()).update(Newpassword.getText().toString()).thenAccept(v -> {
+                    Log.d("user-update", "user updated " + res.getEmail());
+                });
+            });
+        }
+    }
+
+    private void updateName() {
+        if (!name.getText().toString().isEmpty()) {
+            UserInterface.findOne(email).thenAccept(res -> {
+                res.setName(name.getText().toString()).update().thenAccept(v -> {
+                    Log.d("user-update", "user updated " + res.getEmail());
+                });
+            });
+        }
     }
 }
